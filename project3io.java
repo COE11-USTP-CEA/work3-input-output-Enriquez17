3096a2f15558cdfcdadda6a9746b2d8322580fc2
Dan Francis S. Enriquez <yong_enriquez@yahoo.com>
2017-01-27 17:43:23 -0700

    Add files via upload

---
+++ project3io.java
@@ -0,0 +1,10 @@
+class project3io{ 
+2 	public static void main(String[]args){ 
+3 		for(String j: args){ 
+4 			System.out.println("Hello " + j ); 
+5 		} 
+6 		System.out.println("There are " + args.length + "Man"); 
+7 		System.out.println("Hello Everyone!"); 
+8 		System.out.println("I'm Dan Francis Enriquez :)"); 
+9 	} 
+10 } 
